d3feb481950d77fbc7ed099b768feb507ece2c43
imskanand <the.anand3099@gmail.com>
2023-08-29 09:27:11 +0530

    Setting up TypeORM in our application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgresql://postgres:pQmOo6sSiSaaILR0O5CN@containers-us-west-77.railway.app:6642/railway',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
